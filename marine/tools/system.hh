c6ca5e0ecffe6c233b60b45fbf63e1203e93b336
daidodo <daidodo@gmail.com>
2016-10-26 20:25:38 +0800

    fix bug

--- marine/tools/system.hh
+++ marine/tools/system.hh
@@ -22,6 +22,7 @@
 #include <sys/param.h>      //NOFILE
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <signal.h>         //signal
 #include <stdint.h>
 #include <unistd.h>
