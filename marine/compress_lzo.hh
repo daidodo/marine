0cf28582e13c87e6c2cef45b1a84f06f2fae9c45
daidodo <daidodo@gmail.com>
2016-12-17 03:46:03 +0800

    add comments;

--- marine/compress_lzo.hh
+++ marine/compress_lzo.hh
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) 2016 Zhao DAI <daidodo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see accompanying file LICENSE.txt
+ * or <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @brief [miniLZO](http://www.oberhumer.com/opensource/lzo/#minilzo) compression algorithm.
+ * @author Zhao DAI
+ */
+
 #ifndef DOZERG_COMPRESSOR_LZO_H_20080221
 #define DOZERG_COMPRESSOR_LZO_H_20080221
-
-/*
-    LZOÑ¹ËõËã·¨·â×°
-        CCompressorLzo
-//*/
 
 #include <stdint.h>
 #include <vector>
@@ -15,36 +34,64 @@
 
 NS_SERVER_BEGIN
 
+/**
+ * @brief A convenient interface for [miniLZO](http://www.oberhumer.com/opensource/lzo/#minilzo)
+ * compression algorithm.
+ * @note CCompressorLzo object can @em NOT be copied.
+ */
 class CCompressorLzo
 {
     typedef ::lzo_uint  __Uint;
     typedef uint64_t __Len;
 public:
+    /**
+     * @brief Default constructor.
+     */
     CCompressorLzo(){}
+    /**
+     * @name Compression
+     * @{
+     * @brief Compress given data.
+     * If @c output is not empty, the result will append to @c output.
+     * @param[in] input Data to be compressed
+     * @param[out] output A buffer to receive the result
+     * @return @c true if succeeded; otherwise @c false
+     */
     bool compress(const std::vector<char> & input, std::vector<char> & output) const{
         return compressTemplate(input, output);
-    }
-    bool decompress(const std::vector<char> & input, std::vector<char> & output) const{
-        return decompressTemplate(input,output);
     }
     bool compress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
         return compressTemplate(input, output);
     }
-    bool decompress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
-        return decompressTemplate(input, output);
-    }
     bool compress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
         return compressTemplate(input, output);
-    }
-    bool decompress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
-        return decompressTemplate(input, output);
     }
     bool compress(const std::string & input, std::string & output) const{
         return compressTemplate(input, output);
     }
+    /**  @} */
+    /**
+     * @name Decompression
+     * @{
+     * @brief Decompress given data.
+     * If @c output is not empty, the result will append to @c output.
+     * @param[in] input Data to be decompressed
+     * @param[out] output A buffer to receive the result
+     * @return @c true if succeeded; otherwise @c false
+     */
+    bool decompress(const std::vector<char> & input, std::vector<char> & output) const{
+        return decompressTemplate(input,output);
+    }
+    bool decompress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
+        return decompressTemplate(input, output);
+    }
+    bool decompress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
+        return decompressTemplate(input, output);
+    }
     bool decompress(const std::string & input, std::string & output) const{
         return decompressTemplate(input, output);
     }
+    /**  @} */
 private:
     static bool init(){
         static const bool ok = (LZO_E_OK == ::lzo_init());
