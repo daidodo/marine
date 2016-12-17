7c1fa6451193ca87fd344dd83f8a7f3af5f4c707
daidodo <daidodo@gmail.com>
2016-12-17 17:59:10 +0800

    add doxygen comments;

--- marine/charset_convert.hh
+++ marine/charset_convert.hh
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) 2016 Zhao DAI <daidodo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see accompanying file LICENSE.txt
+ * or <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @brief Charset processing and conversion.
+ * @author Zhao DAI
+ */
+
 #ifndef DOZERG_CHARSET_CONVERT_H_20130513
 #define DOZERG_CHARSET_CONVERT_H_20130513
-
-/*
-    字符编码转换
-        CCharsetConvert     支持任意编码间的转换
-//*/
 
 #include <iconv.h>
 #include <cassert>
@@ -12,35 +31,69 @@
 
 NS_SERVER_BEGIN
 
+/**
+ * @brief Converting text between different character encodings.
+ * CCharsetConvert uses @c iconv(3) to perform charset conversion operations. It manages system
+ * resource allocation and deallocation, conversion adjustment and error handling.
+ * @note CCharsetConvert object can @em NOT be copied.
+ */
 class CCharsetConvert
 {
 public:
+    /**
+     * @brief Conversion mode
+     * @li @c kNormal: Default mode;
+     * @li @c kTranslit: Characters that cannot be converted are discarded and an error is printed
+     * after conversion.
+     * @li @c kIgnore: Characters being converted are transliterated when needed and possible.  This
+     * means that when a character cannot be represented in the target character set, it can be
+     * approximated through one or several similar looking characters.  Characters that are outside
+     * of the target character set and cannot be transliterated are replaced with a question mark
+     * (?) in the output.
+     */
     enum EMode{
         kNormal = 0,
         kTranslit,
         kIgnore
     };
+    /**
+     * @brief Construct a default object, with no ability for conversion.
+     * Please use @ref init to initialize this object for use.
+     * @sa init
+     */
     CCharsetConvert()
         : cv_(iconv_t(-1))
         , mode_(kNormal)
     {}
+    /**
+     * @brief Destructor for releasing resources.
+     */
     ~CCharsetConvert(){
         if(valid())
             ::iconv_close(cv_);
     }
-    //初始化
-    //fromCode: 来源数据的编码名，详见iconv --list
-    //toCode: 目标数据的编码名，详见iconv --list
-    //mode: 转码模式
-    //  kNormal     普通模式，遇到目标编码不支持的字符，则返回失败
-    //  kTranslit   使用直译模式，遇到目标编码不支持的字符，使用近似的字符代替
-    //  kIgnore     使用忽略模式，遇到目标编码不支持的字符，则过滤掉
+    /**
+     * @brief Construct a usable converter.
+     * For a full encoding names list supported by your system, please run `iconv --list`.
+     * @param fromCode Encoding name for source text, e.g. "UTF8"
+     * @param toCode Encoding name for destination text, e.g. "GB2312"
+     * @param mode Conversion mode, see @ref EMode
+     */
     CCharsetConvert(const std::string & fromCode, const std::string & toCode, EMode mode = kNormal)
         : cv_(iconv_t(-1))
         , mode_(kNormal)
     {
         init(fromCode, toCode, mode);
     }
+    /**
+     * @brief Initialize a usable converter.
+     * If this object is already initialized, this function will fail.
+     * @n For a full encoding names list supported by your system, please run `iconv --list`.
+     * @param fromCode Encoding name for source text, e.g. "UTF8"
+     * @param toCode Encoding name for destination text, e.g. "GB2312"
+     * @param mode Conversion mode, see @ref EMode
+     * @return @c true if succeeded; otherwise @c false
+     */
     bool init(const std::string & fromCode, const std::string & toCode, EMode mode = kNormal){
         if(valid())
             return false;
@@ -58,20 +111,36 @@ public:
         cv_ = ::iconv_open(t.c_str(), fromCode.c_str());
         return valid();
     }
-    //是否初始化
+    /**
+     * @brief Test whether this object is initialized.
+     * @return @c true if this object is already initialized; otherwise @c false
+     */
     bool valid() const{return (iconv_t(-1) != cv_);}
-    //获取来源编码名
+    /**
+     * @brief Get encoding name for source text.
+     * If this object is not initialized, an empty string will be returned.
+     * @return Encoding name for source text
+     */
     const std::string fromCode() const{return from_;}
-    //获取目标编码名
+    /**
+     * @brief Get encoding name for destination text.
+     * If this object is not initialized, an empty string will be returned.
+     * @return Encoding name for destination text
+     */
     const std::string toCode() const{return to_;}
-    //获取转码模式
+    /**
+     * @brief Get conversion mode.
+     * If this object is not initialized, @c kNormal will be returned.
+     * @return Conversion mode
+     */
     EMode mode() const{return mode_;}
-    //转码
-    //sourc: 来源编码数据
-    //dest: 目标编码数据，新数据将追加到后面
-    //return:
-    //  true    转码成功
-    //  false   转码失败
+    /**
+     * @brief Convert text from source encoding to destination encoding.
+     * If @c dest is not empty, the conversion result will append to @c dest.
+     * @param[in] source Source text
+     * @param[out] dest A String to receive the result of conversion
+     * @return @c true if succeeded; otherwise @c false
+     */
     bool convert(const std::string & source, std::string & dest){
         if(!valid())
             return false;
@@ -94,7 +163,10 @@ public:
         }
         return true;
     }
-    //转化成可读字符串
+    /**
+     * @brief Get a readable description of this object.
+     * @return A readable description of this object
+     */
     std::string toString() const{
         CToString oss;
         oss<<"{cv_="<<cv_
