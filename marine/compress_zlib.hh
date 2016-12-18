0c5c8198984e855147f15efabaafdecfe8e5b152
daidodo <daidodo@gmail.com>
2016-12-18 14:28:20 +0800

    add comments;

--- marine/compress_zlib.hh
+++ marine/compress_zlib.hh
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) 2016 Zhao DAI <daidodo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see accompanying file LICENSE.txt
+ * or <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @brief [zlib](http://www.zlib.net) compression algorithm.
+ * @author Zhao DAI
+ */
+
 #ifndef DOZERG_COMPRESSOR_ZLIB_H_20090223
 #define DOZERG_COMPRESSOR_ZLIB_H_20090223
-
-/*
-    zlibÑ¹ËõËã·¨·â×°
-        CCompressorZlib
-//*/
 
 #include <stdint.h>
 #include <vector>
@@ -16,38 +35,79 @@
 
 NS_SERVER_BEGIN
 
+/**
+ * @brief A convenient interface for [zlib](http://www.zlib.net) compression algorithm.
+ * You may need to link to zlib library (e.g. @c -lz) if you use @c CCompressorZlib.
+ * @note CCompressorZlib object can @em NOT be copied.
+ */
 class CCompressorZlib
 {
     typedef ::uLongf __ZSize;
     typedef ::Bytef  __ZChar;
 public:
-    CCompressorZlib(int level = -1):level_(level){}
+    /**
+     * @brief Constructor.
+     * The compression @c level must be @c Z_DEFAULT_COMPRESSION, or between 0 and 9:
+     * @n @c 1 gives best speed, @c 9 gives best compression, @c 0 gives no compression at all (the
+     * input data is simply copied a block at a time). @c Z_DEFAULT_COMPRESSION requests a default
+     * compromise between speed and compression (currently equivalent to level 6).
+     * @param level Compression level (0 to 9, or @c Z_DEFAULT_COMPRESSION)
+     */
+    CCompressorZlib(int level = Z_DEFAULT_COMPRESSION):level_(level){}
+    /**
+     * @brief Set compression level.
+     * @param lv Compression level (0 to 9, or @c Z_DEFAULT_COMPRESSION)
+     */
     void setLevel(int lv){level_ = lv;}
+    /**
+     * @brief Get compression level.
+     * @return Compression level
+     */
     int getLevel() const{return level_;}
+    /**
+     * @name Compression
+     * @{
+     * @brief Compress given data.
+     * If @c output is not empty, the result will append to @c output.
+     * @param[in] input Data to be compressed
+     * @param[out] output A buffer to receive the result
+     * @return @c true if succeeded; otherwise @c false
+     */
     bool compress(const std::vector<char> & input, std::vector<char> & output) const{
         return compressTemplate(input, output);
-    }
-    bool decompress(const std::vector<char> & input, std::vector<char> & output) const{
-        return decompressTemplate(input, output);
     }
     bool compress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
         return compressTemplate(input, output);
     }
-    bool decompress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
-        return decompressTemplate(input, output);
-    }
     bool compress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
         return compressTemplate(input, output);
-    }
-    bool decompress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
-        return decompressTemplate(input, output);
     }
     bool compress(const std::string & input, std::string & output) const{
         return compressTemplate(input, output);
     }
+    /**  @} */
+    /**
+     * @name Decompression
+     * @{
+     * @brief Decompress given data.
+     * If @c output is not empty, the result will append to @c output.
+     * @param[in] input Data to be decompressed
+     * @param[out] output A buffer to receive the result
+     * @return @c true if succeeded; otherwise @c false
+     */
+    bool decompress(const std::vector<char> & input, std::vector<char> & output) const{
+        return decompressTemplate(input, output);
+    }
+    bool decompress(const std::vector<signed char> & input, std::vector<signed char> & output) const{
+        return decompressTemplate(input, output);
+    }
+    bool decompress(const std::vector<unsigned char> & input, std::vector<unsigned char> & output) const{
+        return decompressTemplate(input, output);
+    }
     bool decompress(const std::string & input, std::string & output) const{
         return decompressTemplate(input, output);
     }
+    /**  @} */
 private:
     template<class Buffer>
     bool compressTemplate(const Buffer & input, Buffer & output) const{
@@ -93,7 +153,7 @@ private:
     CCompressorZlib(const CCompressorZlib &);   //disable copy and assignment
     CCompressorZlib & operator =(const CCompressorZlib &);
     //members
-    int level_;     //compress level of zlib, default to -1 which means level 6
+    int level_;     //compress level of zlib, default to Z_DEFAULT_COMPRESSION, which means level 6
 };
 
 NS_SERVER_END
--- marine/configuration.hh
+++ marine/configuration.hh
@@ -1,13 +1,29 @@
+/*
+ * Copyright (c) 2016 Zhao DAI <daidodo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see accompanying file LICENSE.txt
+ * or <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @brief Configuration file parsing and loading.
+ * @author Zhao DAI
+ */
+
 #ifndef DOZERG_CONFIGURATION_H_20070821
 #define DOZERG_CONFIGURATION_H_20070821
-
-/*
-    配置文件读取
-        CConfiguration
-    TODO:
-        add json support
-        add range support(1,3-10,20,23-100)
-//*/
 
 #include <map>
 #include <string>
@@ -18,31 +34,53 @@
 #include "template.hh"      //CTypeTraits
 #include "tools/string.hh"  //Trim,AbsFilename
 
-/*
-配置文件格式：
-
----- kFormatEqual ----
-[SPACE] # comments [ENDLINE]
-[SPACE] expression [SPACE] = [SPACE] results [SPACE] [# comments] [ENDLINE]
-[SPACE] expression [SPACE] = [SPACE] [# comments] [ENDLINE]
-[SPACE] expression [SPACE] [# comments] [ENDLINE]
-
----- kFormatSpace ----
-[SPACE] # comments [ENDLINE]
-[SPACE] expression [SPACE] results [SPACE] [# comments] [ENDLINE]
-[SPACE] expression [SPACE] [# comments] [ENDLINE]
-
----- kFormatColon ----
-[SPACE] # comments [ENDLINE]
-[SPACE] expression [SPACE] : [SPACE] results [SPACE] [# comments] [ENDLINE]
-[SPACE] expression [SPACE] : [SPACE] [# comments] [ENDLINE]
-[SPACE] expression [SPACE] [# comments] [ENDLINE]
-
-//*/
-
 NS_SERVER_BEGIN
 
 //TODO: re-write unit test
+//TODO: add json support, add range support(1,3-10,20,23-100)
+/**
+ * @brief Parse config file and get attributes.
+ * A config file is a text file consisting of attributes. An attribute consists of a name and its
+ * value.
+ * @n There is only one attribute per line. Empty lines are allowed. Any content after @c # will be
+ * treated as comments, until the end of the line.
+ * @n Attribute name and value consist of any readable characters. Leading and trailing spaces will
+ * be trimmed. But spaces inside of a name or value are considered part of it.
+ * @n CConfiguration supports 3 formats of config files:
+ *
+ * @par @c kFormatEqual
+ * An attribute has the format of `NAME = VALUE`, which means @c = cannot be part of a attribute
+ * name or value.
+ * @n Example:
+ * @code
+ *   # this is an example of kFormatEqual conf file.
+ * first attribute name = first attribute value     # first attribute description
+ *   second:attribute:name = second:attribute:value
+ * third_attribute_name       # with an empty value
+ * @endcode
+ *
+ * @par @c kFormatSpace
+ * An attribute has the format of `NAME  VALUE`, which means @c SPACE cannot be part of a attribute
+ * name or value.
+ * @n Example:
+ * @code
+ *   # this is an example of kFormatSpace conf file.
+ * first=attribute=name     first=attribute=value     # first attribute description
+ *   second:attribute:name  second:attribute:value
+ * third_attribute_name       # with an empty value
+ * @endcode
+ *
+ * @par @c kFormatColon
+ * An attribute has the format of `NAME : VALUE`, which means @c : cannot be part of a attribute
+ * name or value.
+ * @n Example:
+ * @code
+ *   # this is an example of kFormatColon conf file.
+ * first attribute name : first attribute value     # first attribute description
+ *   second attribute name : second attribute value
+ * third_attribute_name       # with an empty value
+ * @endcode
+ */
 class CConfiguration
 {
     typedef std::map<std::string, std::string>   container_type;
@@ -50,11 +88,23 @@ public:
     static const int kFormatEqual = 0;
     static const int kFormatSpace = 1;
     static const int kFormatColon = 2;
-    //得到配置文件名
+    /**
+     * @brief Get config file name.
+     * @return Config file name
+     */
     std::string getConfName() const{return conf_file_;}
-    //清除读入的配置项
+    /**
+     * @brief Clear all attributes loaded.
+     * This function won't change the content of config file.
+     */
     void clear(){content_.clear();}
-    //从文件中加载配置项
+    /**
+     * @brief Parse and load content of a config file.
+     * @param file_name Config file name
+     * @param format Config file format, could be @c kFormatEqual, @c kFormatSpace or @c
+     * kFormatColon
+     * @return @c true if succeeded; otherwise @c false
+     */
     bool load(const std::string & file_name, int format = kFormatSpace){
         std::string abs_file = tools::AbsFilename(file_name);
         std::ifstream inf(abs_file.c_str());
@@ -68,30 +118,60 @@ public:
         conf_file_ = abs_file;
         return true;
     }
-    //得到配置项的字符串值
+    /**
+     * @brief Get string value of an attribute.
+     * @param key Attribute name
+     * @param strdefault Default value if @c key is not found
+     * @return Value of the attribute
+     */
     std::string getString(const std::string & key, const std::string & strdefault = "") const{
         container_type::const_iterator wh = content_.find(key);
         if(wh == content_.end())
             return strdefault;
         return wh->second;
     }
-    //得到配置项的字符串值，如果需要，转换成文件绝对路径
+    /**
+     * @brief Get file name value of an attribute.
+     * This function considers the value to be a file name relative to the current working
+     * directory, and tries to expand it to a full pathname.
+     * @n If the value is empty or a full pathname already, no expansion will take place.
+     * @param key Attribute name
+     * @param strdefault Default value if @c key is not found, @em before expansion
+     * @return Value of the attribute
+     */
     std::string getFilepath(const std::string & key, const std::string & strdefault = "") const{
         const std::string v = getString(key, strdefault);
         return (v.empty() ? v : tools::AbsFilename(v));
     }
-    //得到配置项的int值
+    /**
+     * @name Get integer value of an attribute.
+     * @{
+     * @details These functions try to translate the value to an integer of return type.
+     * @n A number of suffixes (case insensitive) could appear at the end of the value:
+     * | Suffix | Meaning |
+     * | --- | --- |
+     * | @c k or @c kb | @f$N*1024@f$ |
+     * | @c m or @c mb | @f$N*1024^2(=1,048,576)@f$ |
+     * | @c g or @c gb | @f$N*1024^3(=1,073,741,824)@f$ |
+     * | @c t or @c tb | @f$N*1024^4(=1,099,511,627,776)@f$ |
+     * | @c p or @c pb | @f$N*1024^5(=1,125,899,906,842,624)@f$ |
+     * | @c e or @c eb | @f$N*1024^6(=1,152,921,504,606,846,976)@f$ |
+     * For example, @c 2KB is equal to @c 2048.
+     * @n If @c key is not found, @c ndefault will be used as an candidate;
+     * @n If the returned integer would be smaller than @c min, @c min will be returned;
+     * @n If the returned integer would be larger than @c max, @c max will be returned;
+     * @param key Attribute name
+     * @param strdefault Default value if @c key is not found, @em before range checking
+     * @param min Lower boundary of the returned value
+     * @param max Upper boundary of the returned value
+     * @return Value of the attribute
+     */
     int getInt(const std::string & key, int ndefault = 0,
         int min = std::numeric_limits<int>::min(),
         int max = std::numeric_limits<int>::max()) const
     {
         return getInt<int>(key, ndefault, min, max);
     }
-    //得到配置项的整数值
-    //支持格式示例:
-    //  "123"   --->    123
-    //  "123k"  --->    123 * 1024
-    //  "123kb" --->    同上
     template<typename Int>
     Int getInt(const std::string & key, Int ndefault = 0,
         Int min = std::numeric_limits<Int>::min(),
@@ -136,7 +216,11 @@ public:
         }
         return (ret <= min ? min : (ret >= max ? max : ret));
     }
-    //输出可读字符串
+    /**  @} */
+    /**
+     * @brief Get a readable description of this object.
+     * @return A readable description of this object
+     */
     //TODO: unit test
     std::string toString() const{
         CToString oss;
