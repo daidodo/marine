013b0c0f54870cf3847255ae3dba1cd25ae6868f
daidodo <daidodo@gmail.com>
2017-01-02 17:17:50 +0800

    add comments;

--- fd_data_map.hh
+++ fd_data_map.hh
@@ -25,28 +25,6 @@
 #ifndef DOZERG_FD_DATA_MAP_H_20130228
 #define DOZERG_FD_DATA_MAP_H_20130228
 
-/*
-    提供fd到对象的映射
-    以fd作为索引，主要进行边界检查和自动伸缩
-        CFdDataMap  以fd为索引的容器，线程安全
-                    内部存储CSharedPtr<T>，而不是数据指针:w
-    History
-        20080604    CFdSockMap增加对CSharedPtr的支持，主要是putSock的处理不同
-        20080910    CFdSockMap增加sz_字段，Size()函数的意义改为在线fd的个数
-                    GetSock()函数内的null_ptr对象类型加上const限定
-        20080911    CFdSockMap增加CloseSock()函数，避免关闭大量连接时频繁加锁
-                    CFdSockMap增加SetSock()函数的重载，通过一次操作既更换连接对象，又得到旧的连接对象
-                    CFdSockMap修改原SetSock()函数，增加del参数，分离“关闭连接”与“释放对象”操作
-        20080917    给CFdMap::operator [] const增加DEFAULT变量
-                    CFdMap和CFdSockMap增加模板参数Container，决定内部容器类型
-                    CFdMap增加reserve()函数
-        20080920    CFdSockMap使用模板参数决定锁类型
-        20081010    CFdSockMap::GetSock()函数返回值改为__SockPtr对象，因为返回const引用会造成无锁访问map_[fd]
-                    增加CFdSockMap::GetSock(int, __SockPtr &) const重载，减少一个临时对象
-        20081013    增加CFdSockMap::GetSock(ForwardIter, ForwardIter, OutputIter) const重载，批量获取连接对象
-        20130228    重构CFdSockMap，不限于存储Socket对象，并改名为CFdDataMap
-//*/
-
 #include <vector>
 #include <cassert>
 #include "mutex.hh"
@@ -56,9 +34,21 @@
 NS_SERVER_BEGIN
 
 /**
- * @brief
- * @tparam T
- * @tparam LockT
+ * @brief Thread-safe hash table for fd (file descriptor) related data.
+ * Key @em MUST be file descriptors, which are non-negative and of type @c int. In fact the file
+ * descriptor acts as an index to an underlying @c vector of values. Because the number of files a
+ * process can open is limited, e.g. 1024 (it is modifiable), there's no chance for the underlying
+ * @c vector to grow unexpectedly. So do @em NOT use CFdDataMap as a generic @c int to value hash
+ * table.
+ * @n Value can be of any type, even non-copyable types. Usually you want to keep a session for each
+ * socket fd, and the session is non-copyable. CFdDataMap uses smart pointer to hold a value, there
+ * are several advantages. Firstly, it avoids copying issue as mentioned; secondly, it simplifies
+ * lifetime control for each value among multiple threads; And finally, it is efficient for the
+ * underlying @c vector to expand.
+ * @n CFdDataMap is thread safe. You can specify any lock type you want, as long as it cooperates with
+ * CGuard.
+ * @tparam T Value type
+ * @tparam LockT Lock type, default to CSpinLock
  */
 template<class T, class LockT = CSpinLock>
 class CFdDataMap
@@ -71,50 +61,89 @@ private:
     typedef LockT               lock_type;
     typedef CGuard<lock_type>   guard_type;
 public:
-    //ctor
-    //capacity: 预留(fd -> data)个数，可减少内存分配次数
+    /**
+     * @brief Initialize this object.
+     * @c capacity is used as a hint to the number of key/value pairs this object wants to hold. But
+     * CFdDataMap is free to expand as needed.
+     * @param capacity Initial reserved room for key/value pairs
+     */
     explicit CFdDataMap(size_t capacity = 100)
         : map_(capacity)
         , sz_(0)
     {}
-    //获取当前有效的(fd -> data)个数
+    /**
+     * @brief Get number of key/value pairs.
+     * @return Number of key/value pairs
+     */
     size_t size() const{return sz_;}
-    //修改/获取当前可容纳的(fd -> data)个数
+    /**
+     * @brief Get current capacity.
+     * @return Current capacity
+     */
     size_t capacity() const{
         guard_type g(lock_);
         return map_.capacity();
     }
+    /**
+     * @brief Set current capacity.
+     * @param c New capacity
+     */
     void capacity(size_t c){
         guard_type g(lock_);
         map_.capacity(c);
     }
-    //设置fd对应的数据
-    //old: 返回fd之前对应的数据
+    /**
+     * @brief Set value for an fd.
+     * @param[in] fd A file descriptor
+     * @param[in] data New value for @c fd
+     * @param[out] old A smart pointer object to obtain old value for @c fd
+     */
     void setData(int fd, const pointer & data, pointer * old = NULL){
         if(fd < 0)
             return;
         guard_type g(lock_);
         setAux(fd, data, old);
     }
-    //获取fd对应的数据
+    /**
+     * @brief Get value for an fd.
+     * @param fd A file descriptor
+     * @return A smart pointer object hold the value for @c fd
+     */
     pointer getData(int fd) const{
         if(fd < 0)
             return pointer();
         guard_type g(lock_);
         return map_[fd];
     }
-    //获取fd对应的数据，优化版
+    /**
+     * @brief Get value for an fd.
+     * @param[in] fd A file descriptor
+     * @param[out] data A smart pointer object to obtain the value for @c fd
+     */
     void getData(int fd, pointer * data) const{
         if(NULL == data || fd < 0)
             return;
         guard_type g(lock_);
         *data = map_[fd];
     }
-    //批量获取数据
-    //first: fd序列的起始迭代器
-    //last: fd序列的结束迭代器
-    //dstFirst: 输出数据序列的起始迭代器
-    //注意: 调用者负责保证目的地dstFirst的有效性和范围
+    /**
+     * @brief Get values for some file descriptors.
+     * This function saves many lock/unlock operations.
+     * @n Example code:
+     * @code{.cpp}
+     * CFdDataMap<Data> fdmap;
+     *
+     * vector<int> fds;
+     * vector<CFdDataMap<Data>::pointer> values(fds.size());
+     *
+     * fdmap.getData(fds.begin(), fds.end(), values.begin());
+     * @endcode
+     * @param[in] first Begin iterator for a collection of file descriptors
+     * @param[in] last End iterator for a collection of file descriptors
+     * @param[out] dstFirst Begin iterator for a collection of smart pointers to receive the values
+     * of file descriptors
+     * @note The size and validation of destination collection is guaranteed by the user.
+     */
     template<class ForwardIter, class OutputIter>
     void getData(ForwardIter first, ForwardIter last, OutputIter dstFirst) const{
         guard_type g(lock_);
@@ -124,19 +153,23 @@ public:
                 *dstFirst = map_[fd];
         }
     }
-    //移除fd对应的数据
-    //old: 返回fd之前对应的数据
+    /**
+     * @brief Remove value for an fd.
+     * @param[in] fd A file descriptor
+     * @param[out] old A smart pointer object to obtain old value for @c fd, or @c NULL if not
+     * interested
+     */
     void clearData(int fd, pointer * old = NULL){
         if(fd < 0)
             return;
         guard_type g(lock_);
         setAux(fd, pointer(), old);
     }
-    //批量移除数据
-    //first: fd序列的起始迭代器
-    //last: fd序列的结束迭代器
-    //oldFirst: 数据序列的起始迭代器，返回之前的数据
-    //注意: 调用者负责保证目的地dstFirst的有效性和范围
+    /**
+     * @brief Remove values for some file descriptors.
+     * @param[in] first Begin iterator for a collection of file descriptors
+     * @param[in] last End iterator for a collection of file descriptors
+     */
     template<class ForwardIter>
     void clearData(ForwardIter first, ForwardIter last){
         guard_type g(lock_);
@@ -146,6 +179,13 @@ public:
                 setAux(fd, pointer(), NULL);
         }
     }
+    /**
+     * @brief Remove values for some file descriptors.
+     * @param[in] first Begin iterator for a collection of file descriptors
+     * @param[in] last End iterator for a collection of file descriptors
+     * @param[out] dstFirst Begin iterator for a collection of smart pointers to receive old values
+     * @note The size and validation of destination collection is guaranteed by the user.
+     */
     template<class ForwardIter, class OutputIter>
     void clearData(ForwardIter first, ForwardIter last, OutputIter dstFirst){
         guard_type g(lock_);
@@ -155,7 +195,9 @@ public:
                 setAux(fd, pointer(), &*dstFirst);
         }
     }
-    //移除所有数据
+    /**
+     * @brief Clear all key/value pairs.
+     */
     void clear(){
         guard_type g(lock_);
         map_.clear();
