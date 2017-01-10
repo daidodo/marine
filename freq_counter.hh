bfb2233e3ddfa84454e5cefdd48c85564ad17979
daidodo <daidodo@gmail.com>
2017-01-10 09:41:20 +0800

    add comments;

--- freq_counter.hh
+++ freq_counter.hh
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2016 Zhao DAI <daidodo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see accompanying file LICENSE.txt
+ * or <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @brief Events counting and frequency calculation.
+ * @author Zhao DAI
+ */
+
+#ifndef DOZERG_FREQ_COUNTER_H_20170109
+#define DOZERG_FREQ_COUNTER_H_20170109
+
 #include "tools/time.hh"
 #include "to_string.hh"
 
@@ -93,4 +120,6 @@ private:
 };
 
 NS_SERVER_END
+
+#endif
 
